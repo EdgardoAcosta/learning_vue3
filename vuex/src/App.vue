5b89641ae8b8cec88dc327c0b129034ab1d7371a
EdgardoAcosta <acostaleale@gmail.com>
2021-04-05 08:52:58 -0500

    vuex

---
+++ vuex/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 1</button>
+
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from "@/components/TheCounter";
+import FavoriteValue from "@/components/FavoriteValue";
+
+export default {
+  components: {
+    FavoriteValue,
+    TheCounter,
+    BaseContainer,
+  },
+
+  methods: {
+    addOne() {
+      // this.$store.commit('increment');
+      // this.$store.commit('increase', {value: 1});
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 1
+      // });
+      this.$store.dispatch({
+        type: 'increase',
+        value: 1
+      })
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vuex/src/components/FavoriteValue.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>{{ counter }}</h3>
+</template>
+
+<script>
+export default {
+  name: "FavoriteValue",
+  computed: {
+    counter() {
+      return this.$store.getters.normalizeCounter;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
